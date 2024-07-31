3278f7a2e66476144109397355f7943e33841df8
zendParadox <raflirama7446@gmail.com>
2024-07-31 23:34:55 +0700

    creating api for mern-app

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+const app = express();
+const authRoutes = require("./src/routes/auth");
+const blogRoutes = require("./src/routes/blog");
+
+// Middleware for parsing JSON
+app.use(express.json());
+
+// Middleware to enable CORS
+app.use(cors());
+
+// Routes for authentication
+app.use("/v1/auth", authRoutes);
+app.use("/v1/blog", blogRoutes);
+
+// Error handling middleware
+app.use((error, req, res, next) => {
+  const status = error.errorStatus || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    app.listen(4001, () => console.log("Connection Success"));
+  })
+  .catch((err) => console.log(err));
+
+// Error handling middleware for generic errors
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+// Start server on port specified in .env or default to 4000
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ src/controllers/blog.js
@@ -0,0 +1,37 @@
+const { validationResult } = require("express-validator");
+const BlogPost = require("../models/blog");
+
+exports.createBlogPost = (req, res, next) => {
+  const title = req.body.title;
+  // const image = req.body.image
+  const body = req.body.body;
+
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const err = new Error("Input Value Tidak Sesuai");
+    err.errorStatus = 400;
+    err.data = errors.array();
+    throw err;
+  }
+
+  const Posting = new BlogPost({
+    title: title,
+    body: body,
+    author: {
+      uid: 1,
+      name: "Rafli",
+    },
+  });
+
+  Posting.save()
+    .then((result) => {
+      res.status(201).json({
+        message: "Create Blog Post Success",
+        data: result,
+      });
+    })
+    .catch((err) => {
+      console.log("err : ", err);
+    });
+};
